eaef00c6775dbd43210c4e16875b0ab848558f2a
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-01-25 13:54:20 -0300

    Agregado a la app para poder ver el detail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Footer } from "./components/Footer";
 import { HeaderSeccion } from "./components/HeaderSeccion";
 import { SeccionPromociones } from "./components/SeccionPromociones";
 import { ArtContainer } from "./components/ArtContainer";
+//import { ArtContainer2 } from "./components/ArtContainer2";
 
 export function App(){
     return(
