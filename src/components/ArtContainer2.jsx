21bcef3bf102a6f7ab0735c6c12cb4e798cea710
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-01-25 13:50:19 -0300

    Realizado Detalle de producto

---
+++ src/components/ArtContainer2.jsx
@@ -0,0 +1,16 @@
+import { ArtItemDetail } from "./ArtItemDetail";
+
+export function ArtContainer2(){
+    return(
+        <div className="articulos">
+            <ArtItemDetail 
+                codigo="INS400" 
+                ruta="images/servilleta1.jpg" 
+                origen="China" 
+                tamaño="30x30 cm" 
+                stock="20 uni"
+                precio="$U15"
+            />
+        </div>
+    );
+}
