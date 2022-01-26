07ac6be2017db153bfcf2783153e904391576480
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-01-25 22:48:22 -0300

    Actualizacion25012022

--- src/components/ArtContainer2.jsx
+++ src/components/ArtContainer2.jsx
@@ -1,11 +1,12 @@
 import { ArtItemDetail } from "./ArtItemDetail";
+import imagen from '../assets/images/servilleta1.jpg'
 
 export function ArtContainer2(){
     return(
         <div className="articulos">
             <ArtItemDetail 
                 codigo="INS400" 
-                ruta="images/servilleta1.jpg" 
+                ruta={imagen} 
                 origen="China" 
                 tamaño="30x30 cm" 
                 stock="20 uni"
