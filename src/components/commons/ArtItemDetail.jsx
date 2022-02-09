b17575dd1827443c50759c3cdd5174fdf246f940
Joaquin Pedrozo <joaquin.pedrozo@gmail.com>
2022-02-09 16:37:25 -0300

    090220221637

--- src/components/commons/ArtItemDetail.jsx
+++ src/components/commons/ArtItemDetail.jsx
@@ -15,7 +15,7 @@ export function ArtItemDetail(props){
     //Voy a buscar la informacion al servidor
     useEffect(() => {
         //Fetch / GET a JSON Servilletas
-        api.get("/servilletas/" + {id}).then(function (response) {
+        api.get("/servilletas/" + id).then(function (response) {
         const prod = response.data;
         //console.log(prod);    
         //Cambiamos el estado para que react lo re dibuje
